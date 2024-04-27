0a34156d8a78e6d14eb392ef164c38875a0a09bc
ricardojve12 <ricardojve12@gmail.com>
2024-04-27 04:44:25 -0600

    torog

---
+++ TECNOLOGIA_SV/bitsv/index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+?>
+
+<?php
+
+require_once "vistas/parte_superior.php";
+
+
+?>
+
+<div>
+   <h1>BIENVENIDOS!!!</h1>
+
+</div>
+
+
+
+
+
+
+
+<!--fin del contenido princila-->
+
+<?php
+
+require_once "vistas/parte_inferior.php" ?>
