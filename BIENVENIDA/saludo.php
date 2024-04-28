2483d462de2a05bae639113f1ed9f1f2570897b6
ricardojve12 <ricardojve12@gmail.com>
2024-04-28 17:40:06 -0600

    saludo

---
+++ BIENVENIDA/saludo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>el 14 de enero 2024 se les comparte el saludo de Bienvenida el pueblo Salvadoreño comparte el calor de su Naturaleza el AMOR.</h1>
+    
+</body>
+</html>
